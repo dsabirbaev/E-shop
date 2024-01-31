9575070744bfea50ef9fab26854a648a1bf320ea
dsabirbaev <davran9559@gmail.com>
2024-02-01 01:59:28 +0500

    add interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
 
-export default function Home() {
+import Product from "@/components/Product";
+
+
+export default async function Home() {
+  const res = await fetch("https://fakestoreapi.com/products")
+  const products = await res.json();
+
   return (
-    <div>
-      home page
-    </div>
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-52">
+      <section className="flex flex-col space-y-12">
+        <h1 className="text-5xl font-bold text-center">
+           SHOPPING VENUE
+        </h1>
+
+        <div> 
+          {
+            products.map((item) => (
+              <Product key={item.id} product={item}/>
+            ))
+          }
+        </div>
+      </section>
+    </main>
   );
 }
